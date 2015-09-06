f5299af4189b821cd2175319a634d3e8aeb5eadf
Devin Mitchell <devmitchell@gmail.com>
2015-09-05 19:33:01 -0500

    Add concept of admin user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   def can_manage_issue?(issue)
-    self == issue.owner
+    (self == issue.owner) || self.admin?
   end
 end
