cf5d4c213addda4d8a1b5a98fd9942a5572647af
Devin Mitchell <devmitchell@gmail.com>
2015-09-08 18:45:38 -0500

    Basic Gravatar support.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=24&d=mm&r=pg"
+  end
+
 end
